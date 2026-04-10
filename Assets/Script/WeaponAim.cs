f3c93c201e661b3cdeed9d73b3e06f508923d09f
yehuda24 <yehuda.ekglecia@skkkjakarta.sch.id>
2026-04-10 14:02:15 +0700

    Update 2026.4.10
    - Modify Weapon Aim

--- Assets/Script/WeaponAim.cs
+++ Assets/Script/WeaponAim.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class WeaponAim : MonoBehaviour
 {
-    Vector2 mousePosition;
-    public Rigidbody2D rb2d;
+    Vector3 mousePosition;
+   // public Rigidbody2D rb2d;
+    private float rotationSpeed = 100f;
+   // public Transform pivotPoint;
+
 
     // Start is called before the first frame update
     void Update()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);    
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 aimDirection = mousePosition - rb2d.position;
+        Vector3 aimDirection = mousePosition - transform.position;
         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        rb2d.rotation = aimAngle;
+        transform.rotation = Quaternion.Euler(0, 0, aimAngle);
     }
 }
