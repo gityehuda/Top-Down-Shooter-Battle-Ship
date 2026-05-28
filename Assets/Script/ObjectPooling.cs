294bde74567bd9cc2c86b84627582676de2df3f4
Francisco M.P. <9068592+fmpanggabean@users.noreply.github.com>
2026-05-28 16:56:54 +0700

    add object pool untuk bubllet

---
+++ Assets/Script/ObjectPooling.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    public static ObjectPooling instance;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private int poolSize;
+
+    [SerializeField] private List<GameObject> pool = new List<GameObject>();
+
+    private void Awake()
+    {
+        instance = this;
+
+        pool = GeneratePool();
+        HideAllPoolObjects();
+    }
+
+    private void HideAllPoolObjects()
+    {
+        foreach (GameObject obj in pool)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    private List<GameObject> GeneratePool()
+    {
+        List<GameObject> pool = new List<GameObject>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            pool.Add(Instantiate(bulletPrefab, transform));
+        }
+
+        return pool;
+    }
+
+    internal GameObject GetInactiveObject()
+    {
+        foreach(GameObject obj in pool)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                return obj;
+            }
+        }
+        Debug.Log("No pooled object is available right now! Consider expanding the pool size", this);
+        return null;
+    }
+}
